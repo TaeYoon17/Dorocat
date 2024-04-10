b299d76ae8ddd40e05a979ffa48dd2a26c829724
TaeYoon17 <46375289+TaeYoon17@users.noreply.github.com>
2024-04-10 16:10:28 +0900

    Onboarding 제작

---
+++ Dorocat/Scenes/OnboardingViews/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingViews.swift
+//  Dorocat
+//
+//  Created by Developer on 4/10/24.
+//
+
+import SwiftUI
+
+struct OnboardingViews: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    OnboardingViews()
+}
