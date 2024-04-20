e4b83ef3e7d20cda7857fdea47e856453f727812
TaeYoon17 <46375289+TaeYoon17@users.noreply.github.com>
2024-04-20 20:24:02 +0900

    Test) 앱  백그라운드 정보(타이머) 저장 시점 수정

--- Dorocat/Features/TimerFeatures/TimerFeature/Reducers/AppStateReducers.swift
+++ Dorocat/Features/TimerFeatures/TimerFeature/Reducers/AppStateReducers.swift
@@ -24,7 +24,7 @@ extension TimerFeature{
             default: return .none
                 /// 앞으로 background로 이동할 상태
                 /// 현재 진행상황 저장 - background로 이동시 무조건 타이머 상태는 pause가 되도록 설정한다.
-                
+                /*
                 let prevStatus = state.timerStatus // 이전에 갖고 있던 상태를 그대로 저장
                 let pauseStatus = TimerFeatureStatus.getPause(state.timerStatus) ?? state.timerStatus
                 // 이전에 갖고 있던 상태에서 Pause로 이동한 상태를 저장
@@ -36,20 +36,20 @@ extension TimerFeature{
                     await send(.setStatus(pauseStatus))
                     await pomoDefaults.setAll(values)
                 }
-                 
+                 */
             }
         case .background: return .none
-//            let prevStatus = state.timerStatus // 이전에 갖고 있던 상태를 그대로 저장
-//            let pauseStatus = TimerFeatureStatus.getPause(state.timerStatus) ?? state.timerStatus
-//            // 이전에 갖고 있던 상태에서 Pause로 이동한 상태를 저장
-//            let values = PomoValues(status: pauseStatus, information: state.timerInformation, cycle: state.cycle, count: state.count,startDate: state.startDate)
-//            return .run { send in
-//                try await self.setNotification(send: send, status: prevStatus, value: values)
-//                await timeBackground.set(date: Date())
-//                await timeBackground.set(timerStatus: prevStatus)
-//                await send(.setStatus(pauseStatus))
-//                await pomoDefaults.setAll(values)
-//            }
+            let prevStatus = state.timerStatus // 이전에 갖고 있던 상태를 그대로 저장
+            let pauseStatus = TimerFeatureStatus.getPause(state.timerStatus) ?? state.timerStatus
+            // 이전에 갖고 있던 상태에서 Pause로 이동한 상태를 저장
+            let values = PomoValues(status: pauseStatus, information: state.timerInformation, cycle: state.cycle, count: state.count,startDate: state.startDate)
+            return .run { send in
+                try await self.setNotification(send: send, status: prevStatus, value: values)
+                await timeBackground.set(date: Date())
+                await timeBackground.set(timerStatus: prevStatus)
+                await send(.setStatus(pauseStatus))
+                await pomoDefaults.setAll(values)
+            }
         }
     }
     
