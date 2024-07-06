45f149e35644d3b4edd95b2f2f8067e3f60fc7b0
TaeYoon17 <46375289+TaeYoon17@users.noreply.github.com>
2024-07-06 22:27:23 +0900

    고양이 선택 버튼 애니메이션 추가

--- Dorocat/Scenes/TimerViews/TimerComponents/TimerCPT+DoroCat.swift
+++ Dorocat/Scenes/TimerViews/TimerComponents/TimerCPT+DoroCat.swift
@@ -12,26 +12,34 @@ extension TimerViewComponents{
     struct DoroCat:View{
         let store: StoreOf<TimerFeature>
         var body: some View{
-            Group{
-                switch store.timerStatus{
-                case .completed:
-                    LottieView(fileName: store.catType.lottieAssetName(type: .done)
-                               , loopMode: .autoReverse).frame(width: size,height: size)
-                case .breakStandBy:
-                    LottieView(fileName: store.catType.lottieAssetName(type: .done)
-                               , loopMode: .autoReverse)
-                    .frame(width: size,height: size)
-                case .focus,.breakTime,.sleep,.pause:
-                    LottieView(fileName: store.catType.lottieAssetName(type: .sleeping)
-                               , loopMode: .autoReverse)
-                    .frame(width: size,height: size)
-                case .standBy:
-                    LottieView(fileName: store.catType.lottieAssetName(type: .basic), loopMode: .autoReverse)
+            Button(action: {
+            }, label: {
+                Group{
+                    switch store.timerStatus{
+                    case .completed:
+                        LottieView(fileName: store.catType.lottieAssetName(type: .done)
+                                   , loopMode: .autoReverse).frame(width: size,height: size)
+                    case .breakStandBy:
+                        LottieView(fileName: store.catType.lottieAssetName(type: .done)
+                                   , loopMode: .autoReverse)
                         .frame(width: size,height: size)
+                    case .focus,.breakTime,.sleep,.pause:
+                        LottieView(fileName: store.catType.lottieAssetName(type: .sleeping)
+                                   , loopMode: .autoReverse)
+                        .frame(width: size,height: size)
+                    case .standBy:
+                        LottieView(fileName: store.catType.lottieAssetName(type: .basic), loopMode: .autoReverse)
+                            .frame(width: size,height: size)
+                    }
                 }
-            }.onTapGesture {
-                store.send(.viewAction(.catTapped))
-            }
+            }).buttonStyle(CatButtonStyle{
+                Task{
+                    try await Task.sleep(for: .seconds(0.25))
+                    _ = await MainActor.run {
+                        store.send(.viewAction(.catTapped))
+                    }
+                }
+            })
         }
         var size: CGFloat{
             switch store.timerStatus{
@@ -40,4 +48,17 @@ extension TimerViewComponents{
             }
         }
     }
+}
+fileprivate struct CatButtonStyle:ButtonStyle{
+    var willTap:(()->())?
+    func makeBody(configuration: Configuration) -> some View {
+        let translateChagne:CGFloat = configuration.isPressed ? 0.90 : 1
+        configuration.label.scaleEffect(x:translateChagne,y:translateChagne).animation(.spring(duration: 0.2), value: configuration.isPressed)
+            .onChange(of: configuration.isPressed) { oldValue, newValue in
+                if oldValue == false && newValue == true{
+                    willTap?()
+                }
+            }
+    }
+    
 }
